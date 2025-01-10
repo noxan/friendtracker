1c0d240b9c978749097952dcc86f37b9b8099fde
Richard <richard@stromer.org>
2025-01-10 19:03:44 +0100

    use form to add friends

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
 	import { db } from '$lib/db';
 	import { liveQuery } from 'dexie';
 
-	function addFriend() {
-		db.friends.add({
-			name: 'John Doe',
-			age: 30
-		});
+	function onsubmit(event: SubmitEvent) {
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		const name = formData.get('name') as string;
+		const age = parseInt(formData.get('age') as string);
+		db.friends.add({ name, age });
 	}
 
 	let friends = liveQuery(() => db.friends.toArray());
@@ -24,5 +25,11 @@
 		{/if}
 	</ul>
 
-	<button onclick={addFriend}>Add friend</button>
+	<form {onsubmit}>
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" placeholder="Name" />
+		<label for="age">Age</label>
+		<input type="number" name="age" id="age" placeholder="Age" />
+		<button type="submit">Add friend</button>
+	</form>
 </div>
